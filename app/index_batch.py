3d3969476d36c7380e245bd722e45c9c31fa3f89
Sophie Tao <sophietaoyn@gmail.com>
2017-04-12 18:10:15 +0000

    fixed loading es

--- app/index_batch.py
+++ app/index_batch.py
@@ -3,22 +3,20 @@ from elasticsearch import Elasticsearch
 import json
 from time import sleep
 
-print("test\ntest\ntest\ntest\ntest\ntest")
-sleep(20)
-print("haha")
+while True:
+	sleep(20)
+	# Pull messages from kafka
+	consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+	es = Elasticsearch(['es'])
 
-# Pull messages from kafka
-consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
-es = Elasticsearch(['es'])
+	print("before for loop")
+	print(consumer)
+	for message in consumer:
+	    print("in for loop")
+	    new_listing = json.loads((message.value).decode('utf-8'))
+	    # Index the message in elastic search
 
-print("before for loop")
-print(consumer)
-for message in consumer:
-    print("in for loop")
-    new_listing = json.loads((message.value).decode('utf-8'))
-    # Index the message in elastic search
-
-    es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
-    print(new_listing['name'])
-    # Commit the changes to the index files
-    es.indices.refresh(index="listing_index")
+	    es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
+	    print(new_listing['name'])
+	    # Commit the changes to the index files
+	    es.indices.refresh(index="listing_index")
